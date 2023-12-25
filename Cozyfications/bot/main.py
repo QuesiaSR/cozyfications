7bb8a37ecf8ed6df4e2bb71a271688a33c87ec27
D4rk-S0ul <bbittrich0@gmail.com>
2023-12-25 19:18:24 +0100

    Reformatted files according to PEP standard

--- Cozyfications/bot/main.py
+++ Cozyfications/bot/main.py
@@ -1,15 +1,19 @@
-import discord, os
 from datetime import datetime
+
+import discord
+import os
 from discord import ApplicationContext, commands, AutoShardedBot
 from discord.errors import NotFound, Forbidden, HTTPException
+from pyngrok import ngrok
+from twitchAPI import EventSub, Twitch as TwitchAPI, EventSubSubscriptionConflict
+
 from Cozyfications.bot.ui import views
 from Cozyfications.database.classes import StreamerDatabase, TwitchDatabase, MessageDatabase
-from Cozyfications.secrets import Twitch, Ngrok
-from twitchAPI import EventSub, Twitch as TwitchAPI, EventSubSubscriptionConflict
-from pyngrok import ngrok
+from Cozyfications.secrets import Twitch
+
 
 class Callbacks:
-    async def get_guilds(data):
+    async def get_guilds(self, data):
         streamer = data["subscription"]["condition"]["broadcaster_user_id"]
         strdb = StreamerDatabase(streamer)
         guilds = strdb.get_guilds()
@@ -20,72 +24,86 @@ class Callbacks:
             ret.append(TwitchDatabase(guild))
         return ret
 
-    async def update(data):
+    async def update(self, data):
         print("update")
+
         async def callback(data, bot: AutoShardedBot):
-            guilds = await Callbacks.get_guilds(data)
+            guilds = await Callbacks.get_guilds(self, data)
 
             for guild in guilds:
                 guild: TwitchDatabase
                 channel = guild.get_channel()
 
-                if not channel is None:
-                    msgdb = MessageDatabase(guild.guildid, data["subscription"]["condition"]["broadcaster_user_id"], channel)
+                if channel is not None:
+                    msgdb = MessageDatabase(guild.guildid, data["subscription"]["condition"]["broadcaster_user_id"],
+                                            channel)
                     msgid = msgdb.get_message()
 
-                    if msgid != None:
+                    if msgid is not None:
                         sent = await bot.get_channel(channel).fetch_message(int(msgid))
-                        if sent != None:
+                        if sent is not None:
                             await sent.edit("Updated.")
-                        else: print("no msg")
-                    else: print("no msgid")
-                else: print("no channels")
+                        else:
+                            print("no msg")
+                    else:
+                        print("no msgid")
+                else:
+                    print("no channels")
+
         Cozyfications.QUEUE.append({"data": data, "callback": callback})
 
-    async def online(data):
+    async def online(self, data):
         print("online")
+
         async def callback(data, bot: AutoShardedBot):
-            guilds = await Callbacks.get_guilds(data)
+            guilds = await Callbacks.get_guilds(self, data)
 
             for guild in guilds:
                 guild: TwitchDatabase
                 channel = guild.get_channel()
 
-                if not channel is None:
+                if channel is not None:
                     channel = bot.get_channel(channel)
                     message = guild.get_message()
                     message = "Online." if message is None else message
 
                     sent = await channel.send(message)
-                    msgdb = MessageDatabase(guild.guildid, data["subscription"]["condition"]["broadcaster_user_id"], channel)
+                    msgdb = MessageDatabase(guild.guildid, data["subscription"]["condition"]["broadcaster_user_id"],
+                                            channel)
                     msgdb.create_message(sent.id)
+
         Cozyfications.QUEUE.append({"data": data, "callback": callback})
-                
-    async def offline(data):
+
+    async def offline(self, data):
         print("offline")
+
         async def callback(data, bot: AutoShardedBot):
-            guilds = await Callbacks.get_guilds(data)
+            guilds = await Callbacks.get_guilds(self, data)
 
             for guild in guilds:
                 guild: TwitchDatabase
                 channel = guild.get_channel()
 
-                if not channel is None:
+                if channel is not None:
                     message = "Offline."
 
-                    msgdb = MessageDatabase(guild.guildid, data["subscription"]["condition"]["broadcaster_user_id"], channel)
+                    msgdb = MessageDatabase(guild.guildid, data["subscription"]["condition"]["broadcaster_user_id"],
+                                            channel)
                     msgid = msgdb.get_message()
-                    
+
                     channel = bot.get_channel(channel)
-                    if msgid != None:
+                    if msgid is not None:
                         msgdb.delete_message(msgid)
                         try:
                             sent = await channel.fetch_message(int(msgid))
-                            if sent != None:
+                            if sent is not None:
                                 return await sent.edit(message)
-                        except NotFound | Forbidden | HTTPException: pass
+                        except NotFound | Forbidden | HTTPException:
+                            pass
                     await channel.send(message)
+
         Cozyfications.QUEUE.append({"data": data, "callback": callback})
+
 
 class Cozyfications(AutoShardedBot):
     QUEUE = []
@@ -127,22 +145,24 @@ class Cozyfications(AutoShardedBot):
             for extension in ctx.bot.extensions:
                 ctx.bot.reload_extension(extension)
             return await ctx.followup.send(content="Done!")
-        
+
         self.add_application_command(reload_cogs)
 
     async def load_cogs(self, folder=None):
-        if folder != None: self.path = os.path.join(self.path, folder)
+        if folder is not None:
+            self.path = os.path.join(self.path, folder)
         formatted_path = self.path.strip("./").replace("/", ".").replace("\\", ".")
 
         for file in os.listdir(self.path):
             if not os.path.isdir(os.path.join(self.path, file)):
-                if not file in self.cog_blacklist:
+                if file not in self.cog_blacklist:
                     try:
                         self.load_extension(f"{formatted_path}.{file[:-3]}")
                         print(f"  Loaded '{file}'")
-                    except Exception as e: print(e)
+                    except Exception as e:
+                        print(e)
             else:
-                if not file in self.cog_folder_blacklist:
+                if file not in self.cog_folder_blacklist:
                     self.load_cogs(file)
 
     async def run_server(self) -> None:
@@ -153,16 +173,18 @@ class Cozyfications(AutoShardedBot):
         print(f"  Started ngrok server at '{self.url}'")
 
     async def subscribe(self, user, guildid) -> None:
-        if self.hook != None:
+        if self.hook is not None:
             self.new_subscriptions += 1
             for subscription in self.subscriptions:
                 try:
-                    subid = self.hook._subscribe(subscription, "1", {"broadcaster_user_id": str(user)}, self.subscriptions[subscription])
+                    subid = self.hook._subscribe(subscription, "1", {"broadcaster_user_id": str(user)},
+                                                 self.subscriptions[subscription])
                     StreamerDatabase(user).add_guild(guildid, subid)
-                except EventSubSubscriptionConflict: pass
+                except EventSubSubscriptionConflict:
+                    pass
 
     async def unsubscribe(self, user, guildid) -> None:
-        if self.ttv != None:
+        if self.ttv is not None:
             self.del_subscriptions += 1
             strdb = StreamerDatabase(user)
             subids = strdb.get_subids(guildid)
@@ -177,14 +199,15 @@ class Cozyfications(AutoShardedBot):
         self.hook = EventSub(self.url, Twitch.ID, self.port, self.ttv)
         (self.hook.unsubscribe_all(), print("  Unsubscribed from all events"))
         (self.hook.start(), print("  Started event hook"))
-    
+
     async def subscribe_events(self):
         print("  Fetching users...")
         users = StreamerDatabase.get_streamers()
-        
+
         for user in users:
             for subscription in self.subscriptions:
-                self.hook._subscribe(subscription, "1", {"broadcaster_user_id": str(user)}, self.subscriptions[subscription])
+                self.hook._subscribe(subscription, "1", {"broadcaster_user_id": str(user)},
+                                     self.subscriptions[subscription])
                 if user == users[len(users) - 1]:
                     print(f"  Finished subscribing to '{subscription}' for all users")
         print("  Finished subscribing to all subscriptions for all users")
@@ -219,6 +242,7 @@ class Cozyfications(AutoShardedBot):
             self.persistent_views_added = True
 
         return print(f"Ready, took {(datetime.utcnow() - self.start_time).seconds} seconds.")
+
 
 if __name__ == "__main__":
     exit("The bot cannot be run directly from the bot file.")
--- Cozyfications/database/classes.py
+++ Cozyfications/database/classes.py
@@ -1,65 +1,179 @@
 from Cozyfications import database
 
+
 class TwitchDatabase:
     def __init__(self, guildid):
         self.guildid = guildid
-    
-    def exists(self): return database.select(f"SELECT `guildid` FROM `cozyfications`.`twitch` WHERE guildid = '{self.guildid}'").value != None
 
-    def get_streamers(self): return database.select(f"SELECT `streamers` FROM `cozyfications`.`twitch` WHERE guildid = '{self.guildid}'").value
+    def exists(self):
+        return database.select(
+            f"""SELECT `guildid`
+            FROM `cozyfications`.`twitch`
+            WHERE guildid = '{self.guildid}'"""
+        ).value is not None
+
+    def get_streamers(self):
+        return database.select(
+            f"""SELECT `streamers`
+            FROM `cozyfications`.`twitch`
+            WHERE guildid = '{self.guildid}'"""
+        ).value
+
     def add_streamer(self, streamer: str):
         streamers = self.get_streamers()
-        if not self.exists(): database.update(f"INSERT INTO `cozyfications`.`twitch` (`guildid`, `streamers`) VALUES ('{self.guildid}', '[\"{streamer}\"]')")
+        if not self.exists():
+            database.update(
+                f"""INSERT INTO `cozyfications`.`twitch` (`guildid`, `streamers`)
+                VALUES ('{self.guildid}', '[\"{streamer}\"]')"""
+            )
         else:
             streamers = [] if streamers is None else streamers
             streamers.append(streamer)
             streamers = str(streamers).replace("'", '"')
-            database.update(f"UPDATE `cozyfications`.`twitch` SET `streamers` = '{streamers}' WHERE (`guildid` = '{self.guildid}')")
+            database.update(
+                f"""UPDATE `cozyfications`.`twitch`
+                SET `streamers` = '{streamers}'
+                WHERE (`guildid` = '{self.guildid}')"""
+            )
+
     def remove_streamer(self, streamer: str):
         streamers = self.get_streamers()
         streamers.remove(streamer)
         streamers = str(streamers).replace("'", '"')
-        if streamers != None: database.update(f"UPDATE `cozyfications`.`twitch` SET `streamers` = '{streamers}' WHERE (`guildid` = '{self.guildid}')")
-    
-    def get_message(self): return database.select(f"SELECT `message` FROM `cozyfications`.`twitch` WHERE guildid = '{self.guildid}'").value
-    def set_message(self, message: str=None):
-        if not self.exists(): database.update(f"INSERT INTO `cozyfications`.`twitch` (`guildid`, `message`) VALUES ('{self.guildid}', '{message}')")
-        else: database.update(f"UPDATE `cozyfications`.`twitch` SET `message` = '{message}' WHERE (`guildid` = '{self.guildid}')")
+        if streamers is not None:
+            database.update(
+                f"""UPDATE `cozyfications`.`twitch`
+                SET `streamers` = '{streamers}'
+                WHERE (`guildid` = '{self.guildid}')"""
+            )
+
+    def get_message(self):
+        return database.select(
+            f"""SELECT `message`
+            FROM `cozyfications`.`twitch`
+            WHERE guildid = '{self.guildid}'"""
+        ).value
+
+    def set_message(self, message: str = None):
+        if not self.exists():
+            database.update(
+                f"""INSERT INTO `cozyfications`.`twitch` (`guildid`, `message`)
+                VALUES ('{self.guildid}', '{message}')"""
+            )
+        else:
+            database.update(
+                f"""UPDATE `cozyfications`.`twitch`
+                SET `message` = '{message}'
+                WHERE (`guildid` = '{self.guildid}')"""
+            )
+
     def remove_message(self):
         if self.exists():
-            database.update(f"UPDATE `cozyfications`.`twitch` SET `message` = '{None}' WHERE (`guildid` = '{self.guildid}')")
-    
-    def get_channel(self): return database.select(f"SELECT `channel` FROM `cozyfications`.`twitch` WHERE guildid = '{self.guildid}'").value
-    def set_channel(self, channel: int=None):
-        if not self.exists(): database.update(f"INSERT INTO `cozyfications`.`twitch` (`guildid`, `channel`) VALUES ('{self.guildid}', '{channel}')")
-        else: database.update(f"UPDATE `cozyfications`.`twitch` SET `channel` = '{channel}' WHERE (`guildid` = '{self.guildid}')")
+            database.update(
+                f"""UPDATE `cozyfications`.`twitch`
+                SET `message` = '{None}'
+                WHERE (`guildid` = '{self.guildid}')"""
+            )
+
+    def get_channel(self):
+        return database.select(
+            f"""SELECT `channel`
+            FROM `cozyfications`.`twitch`
+            WHERE guildid = '{self.guildid}'"""
+        ).value
+
+    def set_channel(self, channel: int = None):
+        if not self.exists():
+            database.update(
+                f"""INSERT INTO `cozyfications`.`twitch` (`guildid`, `channel`)
+                VALUES ('{self.guildid}', '{channel}')"""
+            )
+        else:
+            database.update(
+                f"""UPDATE `cozyfications`.`twitch`
+                SET `channel` = '{channel}'
+                WHERE (`guildid` = '{self.guildid}')"""
+            )
+
     def remove_channel(self):
         if self.exists():
-            database.update(f"UPDATE `cozyfications`.`twitch` SET `channel` = '{None}' WHERE (`guildid` = '{self.guildid}')")
+            database.update(
+                f"""UPDATE `cozyfications`.`twitch`
+                SET `channel` = '{None}'
+                WHERE (`guildid` = '{self.guildid}')"""
+            )
+
 
 class StreamerDatabase:
     def __init__(self, streamer):
         self.streamer = streamer
-    
-    @staticmethod
-    def get_streamers(): return database.select(f"SELECT `streamer` FROM `cozyfications`.`subscriptions`").value_all
-    
-    def get_subids(self, guildid): return database.select(f"SELECT `subid` FROM `cozyfications`.`subscriptions` WHERE `streamer` = '{self.streamer}' AND guildid = '{guildid}'").value_all
-    def get_guilds(self): return database.select(f"SELECT `guildid` FROM `cozyfications`.`subscriptions` WHERE `streamer` = '{self.streamer}'").value_all
 
-    def add_guild(self, guildid, subid): database.update(f"INSERT INTO `cozyfications`.`subscriptions` (`streamer`, `guildid`, `subid`) VALUES ('{self.streamer}', '{guildid}', '{subid}')")
-    def remove_guild(self, guildid): database.update(f"DELETE FROM `cozyfications`.`subscriptions` WHERE `streamer` = '{self.streamer}' AND guildid = '{guildid}'")
+    @staticmethod
+    def get_streamers():
+        return database.select(
+            f"""SELECT `streamer`
+            FROM `cozyfications`.`subscriptions`"""
+        ).value_all
+
+    def get_subids(self, guildid):
+        return database.select(
+            f"""SELECT `subid`
+            FROM `cozyfications`.`subscriptions`
+            WHERE `streamer` = '{self.streamer}' AND guildid = '{guildid}'"""
+        ).value_all
+
+    def get_guilds(self):
+        return database.select(
+            f"""SELECT `guildid`
+            FROM `cozyfications`.`subscriptions`
+            WHERE `streamer` = '{self.streamer}'"""
+        ).value_all
+
+    def add_guild(self, guildid, subid):
+        database.update(
+            f"""INSERT INTO `cozyfications`.`subscriptions` (`streamer`, `guildid`, `subid`)
+            VALUES ('{self.streamer}', '{guildid}', '{subid}')"""
+        )
+
+    def remove_guild(self, guildid):
+        database.update(
+            f"""DELETE FROM `cozyfications`.`subscriptions`
+            WHERE `streamer` = '{self.streamer}' AND guildid = '{guildid}'"""
+        )
+
 
 class MessageDatabase:
     def __init__(self, guildid, streamer, channelid):
         self.guildid = guildid
         self.streamer = streamer
         self.channelid = channelid
-    
-    def get_message(self): return database.select(f"SELECT `messageid` FROM `cozyfications`.`messages` WHERE guildid = '{self.guildid}' AND streamer = '{self.streamer}' AND channelid = '{self.channelid}'").value
-    def exists(self) -> bool: return self.get_message() != None
+
+    def get_message(self):
+        return database.select(
+            f"""SELECT `messageid`
+            FROM `cozyfications`.`messages`
+            WHERE guildid = '{self.guildid}' AND streamer = '{self.streamer}' AND channelid = '{self.channelid}'"""
+        ).value
+
+    def exists(self) -> bool:
+        return self.get_message() is not None
+
     def create_message(self, messageid):
         if not self.exists():
-            return database.update(f"INSERT INTO `cozyfications`.`messages` (`messageid`, `guildid`, `streamer`, `channelid`) VALUES ('{messageid}', '{self.guildid}', '{self.streamer}', '{self.channelid}')")
-        return database.update(f"UPDATE `cozyfications`.`messages` SET `messageid` = '{messageid}' WHERE (`guildid` = '{self.guildid}' and `streamer` = '{self.streamer}' and `channelid` = '{self.channelid}')")
-    def delete_message(self, messageid): database.update(f"DELETE FROM `cozyfications`.`messages` WHERE `messageid` = '{messageid}' AND `guildid` = '{self.guildid}' AND `streamer` = '{self.streamer}' AND `channelid` = '{self.channelid}'")
+            return database.update(
+                f"""INSERT INTO `cozyfications`.`messages` (`messageid`, `guildid`, `streamer`, `channelid`)
+                VALUES ('{messageid}', '{self.guildid}', '{self.streamer}', '{self.channelid}')"""
+            )
+        return database.update(
+            f"""UPDATE `cozyfications`.`messages`
+            SET `messageid` = '{messageid}'
+            WHERE (`guildid` = '{self.guildid}' AND `streamer` = '{self.streamer}'
+            AND `channelid` = '{self.channelid}')"""
+        )
+
+    def delete_message(self, messageid):
+        database.update(
+            f"""DELETE FROM `cozyfications`.`messages`
+            WHERE `messageid` = '{messageid}' AND `guildid` = '{self.guildid}'
+            AND `streamer` = '{self.streamer}' AND `channelid` = '{self.channelid}'"""
+        )
