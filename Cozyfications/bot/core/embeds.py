e83e68b6c5bfb262ef02eb98e23fd837e6db30e8
DarkSoul <83767949+D4rk-S0ul@users.noreply.github.com>
2024-01-05 15:21:22 +0100

    Changed help embed layout

--- Cozyfications/bot/core/embeds.py
+++ Cozyfications/bot/core/embeds.py
@@ -254,7 +254,7 @@ class HelpSelectEmbed(CozyficationsEmbed):
         super().__init__(bot=bot, **kwargs)
         self.title: str = f"{self.cog.__cog_name__} Commands"
         self.description: str = "\n".join(
-            f"`/{command.qualified_name}`: {command.description}"
+            f"</{command.qualified_name}:{command.qualified_id}>: {command.description}"
             for command in self.cog.walk_commands()
         )
 
