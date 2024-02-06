22dc1ca7e9ba119657dd3f6501f47dd06e0efbd2
DarkSoul <83767949+D4rk-S0ul@users.noreply.github.com>
2024-02-06 16:38:31 +0100

    Reverted secrets.py

--- Cozyfications/secrets.py
+++ Cozyfications/secrets.py
@@ -6,6 +6,8 @@ dotenv.load_dotenv()
 
 
 class Discord:
+    ID = os.getenv("DISCORD_ID")
+    SECRET = os.getenv("DISCORD_SECRET")
     TOKEN = os.getenv("DISCORD_TOKEN")
     ERRORS_WEBHOOK = os.getenv("DISCORD_ERRORS_WEBHOOK")
 
@@ -13,3 +15,10 @@ class Discord:
 class Twitch:
     ID = os.getenv("TWITCH_ID")
     SECRET = os.getenv("TWITCH_SECRET")
+
+
+class Database:
+    HOST = os.getenv("DB_HOST")
+    PORT = os.getenv("DB_PORT")
+    USER = os.getenv("DB_USER")
+    PASSWORD = os.getenv("DB_PASSWORD")
